cff21e87aaa95d533a111a2be6fed828a1f84942
Alice <aliceagottlieb@gmail.com>
2026-05-06 17:50:44 -0700

    Add qsub basic template

---
+++ templates/qsub-basic-template.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -cwd
+#$ -o {OUTPUT_LOG_FILE_NAME}
+#$ -j y
+#$ -l {RESOURCES} {e.g. h_vmem=4G,h_rt=}
+#$ -M $USER@ucla.edu
+#$ -m bea
+
+# Source module system and user profile
+. /u/local/Modules/default/init/modules.sh
+source ~/.bashrc
+
+# Your Code here
